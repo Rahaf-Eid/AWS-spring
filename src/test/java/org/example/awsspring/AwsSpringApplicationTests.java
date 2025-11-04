de4b7601bc10df4fd443eee5fbc0c25532c932ef
Rahaf Eid <rahaf.eid.202@gmail.com>
2025-11-04 15:13:40 +0300

    Initial commit

---
+++ src/test/java/org/example/awsspring/AwsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.awsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
